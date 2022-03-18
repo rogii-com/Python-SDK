04edad44192d5351f9f4dc5371f50e89f9389790
N.Sapegin <n.sapegin@rogii.com>
2022-03-18 11:15:17 +0400

    SC-731 Add solo_username and solo_password

--- python-sdk/api.py
+++ python-sdk/api.py
@@ -1,12 +1,15 @@
 from typing import Optional
 
 class PyRogii:
-
     def __init__(self,
                  project_id: int,
                  client_id: int,
-                 client_secret: Optional[str] = None):
-
+                 client_secret: Optional[str] = None,
+                 solo_username: Optional[str] = None,
+                 solo_password: Optional[str] = None
+                ):
         self.project_id = project_id
         self.client_id = client_id
         self.client_secret = client_secret
+        self.solo_username = solo_username
+        self.solo_password = solo_password
