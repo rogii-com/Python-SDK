026cef80f12f8b99c41d4b2c4d2ad96b9ba8070e
N.Sapegin <n.sapegin@rogii.com>
2022-04-08 12:55:59 +0400

    SC-732 code formatting fixed

--- tests/test_python_sdk_client.py
+++ tests/test_python_sdk_client.py
@@ -4,12 +4,12 @@ from python_sdk.client import PyRogii
 
 
 pr = PyRogii(
-        client_id=environ.get('CLIENT_ID'),
-        client_secret=environ.get('CLIENT_SECRET'),
-        solo_username=environ.get('SOLO_USERNAME'),
-        solo_password=environ.get('SOLO_PASSWORD'),
-        papi_domain_name=environ.get('PAPI_DOMAIN_NAME')
-    )
+    client_id=environ.get('CLIENT_ID'),
+    client_secret=environ.get('CLIENT_SECRET'),
+    solo_username=environ.get('SOLO_USERNAME'),
+    solo_password=environ.get('SOLO_PASSWORD'),
+    papi_domain_name=environ.get('PAPI_DOMAIN_NAME')
+)
 
 
 def test_auth():
