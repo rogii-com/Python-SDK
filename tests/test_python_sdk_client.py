589b3cae9a69bada9e318904d9a4f129526e64cd
Timur Mestan-zade <t.mestan-zade@rogii.com>
2022-06-14 18:27:28 +0400

    Fix tests

--- tests/test_python_sdk_client.py
+++ tests/test_python_sdk_client.py
@@ -1,3 +1,4 @@
+import pytest
 import random
 from os import environ
 
@@ -12,12 +13,11 @@ nested_well_name = 'Nested Well'
 pr = PyRogii(
     client_id=environ.get('CLIENT_ID'),
     client_secret=environ.get('CLIENT_SECRET'),
-    project_name=project_name,
     solo_username=environ.get('SOLO_USERNAME'),
     solo_password=environ.get('SOLO_PASSWORD'),
     papi_domain_name=environ.get('PAPI_DOMAIN_NAME')
 )
-
+pr.set_project(project_name)
 
 def test_auth():
     assert pr._papi_client is not None
@@ -43,29 +43,27 @@ def test_get_project_wells():
 
 
 def test_get_well():
-    assert pr.get_well(well_name=well_name)['name'] == well_name
+    assert pr.get_well(well_name=well_name).at[0, 'name'] == well_name
 
 
 def test_get_well_interpretation():
-    assert pr.get_well_interpretation(
-        well_name=well_name, interpretation_name=interpretation_name
-    )['meta']['name'] == interpretation_name
+    interpretation = pr.get_well_interpretation(well_name=well_name, interpretation_name=interpretation_name)
+    assert interpretation['meta'].at[0, 'name'] == interpretation_name
 
 
 def test_get_well_starred_interpretation():
-    assert pr.get_well_interpretation(
-        well_name=well_name, interpretation_name=interpretation_name
-    )['meta']['name'] == interpretation_name
-
-
-def test_get_well_starred_target_line():
-    assert pr.get_well_starred_target_line(well_name=well_name)['name'] == target_line_name
+    interpretation = pr.get_well_starred_interpretation(well_name=well_name)
+    assert interpretation['meta'].at[0, 'name'] == interpretation_name
 
 
 def test_get_well_target_line():
-    assert pr.get_well_target_line(
-        well_name=well_name, target_line_name=target_line_name
-    )['name'] == target_line_name
+    target_line = pr.get_well_target_line(well_name=well_name, target_line_name=target_line_name)
+    assert target_line.at[0, 'name'] == target_line_name
+
+
+def test_get_well_starred_target_line():
+    target_line = pr.get_well_starred_target_line(well_name=well_name)
+    assert target_line.at[0, 'name'] == target_line_name
 
 
 def test_get_well_target_lines():
@@ -81,18 +79,17 @@ def test_get_well_nested_wells():
 
 
 def test_get_well_nested_well():
-    assert pr.get_well_nested_well(
-        well_name=well_name, nested_well_name=nested_well_name
-    )['name'] == nested_well_name
+    nested_well = pr.get_well_nested_well(well_name=well_name, nested_well_name=nested_well_name)
+    assert nested_well.at[0, 'name'] == nested_well_name
 
 
 def test_create_nested_well():
-    _nested_well_name = 'Nested Well ' + str(random.randint(0, 10000))
+    nested_well_name = 'Nested Well ' + str(random.randint(0, 10000))
     pr.create_nested_well(
         well_name=well_name,
-        nested_well_name=_nested_well_name,
+        nested_well_name=nested_well_name,
         operator='Operator',
-        api=_nested_well_name,
+        api=nested_well_name,
         xsrf=100000.0,
         ysrf=100000.0,
         kb=0.0,
@@ -100,6 +97,4 @@ def test_create_nested_well():
         tie_in_ns=0.0,
         tie_in_ew=0.0
     )
-    assert pr.get_well_nested_well(
-        well_name=well_name, nested_well_name=_nested_well_name
-    ) is not None
+    assert pr.get_well_nested_well(well_name=well_name, nested_well_name=nested_well_name) is not None
