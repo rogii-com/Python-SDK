54c0ed0f7aa0a398ffbc45571924098e316acf3f
Timur Mestan-zade <t.mestan-zade@rogii.com>
2022-07-11 13:12:54 +0400

    SC-935: Replace lambdas with simple functions

--- tests/mocks/solo_client.py
+++ tests/mocks/solo_client.py
@@ -13,22 +13,50 @@ from tests.mocks.papi_data import (
 )
 
 
+def fetch_projects(**kwargs):
+    return PROJECTS_DATA_RESPONSE
+
+
+def fetch_virtual_projects(**kwargs):
+    return VIRTUAL_PROJECTS_DATA_RESPONSE
+
+
+def fetch_project_wells(**kwargs):
+    return WELLS_DATA_RESPONSE
+
+
 def fetch_well_raw_trajectory(**kwargs):
     return TRAJECTORY_DATA_RESPONSE['content']
+
+
+def fetch_well_raw_interpretations(**kwargs):
+    return INTERPRETATIONS_DATA_RESPONSE
+
+
+def fetch_interpretation_horizons(**kwargs):
+    return HORIZONS_DATA_RESPONSE
 
 
 def fetch_interpretation_assembled_segments(**kwargs):
     return ASSEMBLED_SEGMENTS_DATA_RESPONSE['assembled_segments']
 
 
+def fetch_well_target_lines(**kwargs):
+    return TARGET_LINES_DATA_RESPONSE
+
+
+def fetch_well_nested_wells(**kwargs):
+    return NESTED_WELLS_DATA_RESPONSE
+
+
 solo_client = SoloClient(client_id='client_id', client_secret='client_secret')
 
-solo_client._papi_client.fetch_projects = lambda **kwargs: PROJECTS_DATA_RESPONSE
-solo_client._papi_client.fetch_virtual_projects = lambda **kwargs: VIRTUAL_PROJECTS_DATA_RESPONSE
-solo_client._papi_client.fetch_project_wells = lambda **kwargs: WELLS_DATA_RESPONSE
+solo_client._papi_client.fetch_projects = fetch_projects
+solo_client._papi_client.fetch_virtual_projects = fetch_virtual_projects
+solo_client._papi_client.fetch_project_wells = fetch_project_wells
 solo_client._papi_client.fetch_well_raw_trajectory = fetch_well_raw_trajectory
-solo_client._papi_client.fetch_well_raw_interpretations = lambda **kwargs: INTERPRETATIONS_DATA_RESPONSE
-solo_client._papi_client.fetch_interpretation_horizons = lambda **kwargs: HORIZONS_DATA_RESPONSE
+solo_client._papi_client.fetch_well_raw_interpretations = fetch_well_raw_interpretations
+solo_client._papi_client.fetch_interpretation_horizons = fetch_interpretation_horizons
 solo_client._papi_client.fetch_interpretation_assembled_segments = fetch_interpretation_assembled_segments
-solo_client._papi_client.fetch_well_target_lines = lambda **kwargs: TARGET_LINES_DATA_RESPONSE
-solo_client._papi_client.fetch_well_nested_wells = lambda **kwargs: NESTED_WELLS_DATA_RESPONSE
+solo_client._papi_client.fetch_well_target_lines = fetch_well_target_lines
+solo_client._papi_client.fetch_well_nested_wells = fetch_well_nested_wells
