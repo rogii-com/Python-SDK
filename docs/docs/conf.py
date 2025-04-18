52aa98f21b4e0f095123412a8d5a095516359e3d
Polina Alekseeva <polina.alekseeva@rogii.com>
2025-04-18 09:46:02 +0400

    PYT-846: Added ignore flake for importing path

--- docs/docs/conf.py
+++ docs/docs/conf.py
@@ -10,8 +10,9 @@ curr_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, curr_path)
 sys.path.insert(0, os.path.abspath(os.path.join(curr_path, '../../src')))
 
-from custom_stuff.skip_methods import skip_methods
-from custom_stuff.navigation_settings import sidebar_settings
+# noqa: E402 — отключаем flake8 ругань только тут
+from custom_stuff.skip_methods import skip_methods  # noqa: E402
+from custom_stuff.navigation_settings import sidebar_settings  # noqa: E402
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
