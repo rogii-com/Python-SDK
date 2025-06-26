5d339e6c09ad0e17ab5ad2784d5bc5cb9f021a93
polina.alekseeva <polina.alekseeva@rogii.com>
2025-06-26 13:29:49 +0500

    PYT-916: Create SDK documentation for the production

--- docs/docs/custom_stuff/skip_methods.py
+++ docs/docs/custom_stuff/skip_methods.py
@@ -1,3 +1,12 @@
+import os
+import re
+
+THEME_SCRIPT = re.compile(
+    r'<script>\s*document\.body\.dataset\.theme\s*=\s*localStorage\.getItem\("theme"\)\s*\|\|\s*"auto";\s*</script>',
+    re.DOTALL,
+)
+
+
 def skip_methods(app, what, name, obj, skip, options):
     if what in ('method', 'attribute', 'data', 'attributes'):
         method_name = name.rsplit('.', 1)[-1]
@@ -12,3 +21,26 @@ def skip_methods(app, what, name, obj, skip, options):
         return True
 
     return None
+
+
+def skip_inline_theme_script(app, exception):
+    if exception or app.builder.name != 'html':
+        return
+
+    outdir = app.builder.outdir
+
+    for root, _, files in os.walk(outdir):
+        for fname in files:
+            if not fname.endswith('.html'):
+                continue
+
+            path = os.path.join(root, fname)
+
+            with open(path, 'r', encoding='utf-8') as f:
+                text = f.read()
+
+            new_text = THEME_SCRIPT.sub('', text)
+
+            if text != new_text:
+                with open(path, 'w', encoding='utf-8') as f:
+                    f.write(new_text)
